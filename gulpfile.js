490e0122e70c8874f91db83226e783c34a4d918e
davidleston <david.leston@gmail.com>
2016-06-05 16:41:04 -0700

    (MIT) license removed from CSS.

--- gulpfile.js
+++ gulpfile.js
@@ -60,7 +60,11 @@ gulp.task('html', ['styles', 'scripts'], () => {
   return gulp.src('app/*.html')
     .pipe($.useref({searchPath: ['.tmp', 'app', '.']}))
     .pipe($.if('*.js', $.uglify()))
-    .pipe($.if('*.css', $.cssnano({safe: true, autoprefixer: false})))
+    .pipe($.if('*.css', $.cssnano({
+      safe: true,
+      autoprefixer: false,
+      discardComments: {removeAll: true}
+    })))
     .pipe($.if('*.html', $.htmlmin({
       collapseWhitespace: true,
       collapseBooleanAttributes: true,
