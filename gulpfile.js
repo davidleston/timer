348cea990e1be5aee4ebc69eeda506366eefab57
davidleston <david.leston@gmail.com>
2016-06-05 17:03:42 -0700

    Unused CSS removed.

--- gulpfile.js
+++ gulpfile.js
@@ -60,6 +60,10 @@ gulp.task('html', ['styles', 'scripts'], () => {
   return gulp.src('app/*.html')
     .pipe($.useref({searchPath: ['.tmp', 'app', '.']}))
     .pipe($.if('*.js', $.uglify()))
+    .pipe($.if('*.css', $.uncss({
+      html: ['app/*.html'],
+      ignore: [/.*has-error.*/]
+    })))
     .pipe($.if('*.css', $.cssnano({
       safe: true,
       autoprefixer: false,
